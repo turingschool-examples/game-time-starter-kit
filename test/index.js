ce41d7545d2e56c9c5ad9d88d3f17927a925375b
worace <horace.d.williams@gmail.com>
2015-09-02 17:00:25 -0600

    some examples

--- test/index.js
+++ test/index.js
@@ -5,4 +5,14 @@ describe('my test suite', function () {
   it('should work', function () {
     assert(true);
   });
+
+  it("renders a game", function() {
+    var g = new Game();
+    var $gameElement = $("#game-goes-here");
+    g.render($gameElement);
+    var renderedGame = $(".game");
+    assert.equal(1, renderedGame.length)
+    var cards = $(".cards")
+    assert.equal(8, cards.length)
+  })
 });
