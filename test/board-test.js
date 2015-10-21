98ef88db46b1a0ff9e5d1f5038d5952fb564aee6
mrjaimisra <misrajai01@gmail.com>
2015-10-21 14:42:06 -0600

    Add freeSpaces method to Board object

--- test/board-test.js
+++ test/board-test.js
@@ -18,7 +18,10 @@ describe('the board', function () {
   it('spaces should be a four by four matrix', function () {
     let board = new Board();
     let spaces = board.spaces;
-    let expected = [ [], [], [], [] ];
+    let expected = [[null, null, null, null],
+                    [null, null, null, null],
+                    [null, null, null, null],
+                    [null, null, null, null]];
 
     assert.deepEqual(spaces, expected)
   });
@@ -27,7 +30,24 @@ describe('the board', function () {
     let game = new Game();
     let board = new Board(game);
 
-    assert.equal(board.game, game); 
+    assert.equal(board.game, game);
   });
-  
+
+  describe('freeSpaces', function () {
+
+    it('should return an array of positions of available spaces', function () {
+      let board = new Board('game');
+      let freeSpaces = board.freeSpaces();
+
+      // [ columnIndex, rowIndex ]
+      let expected = [ [0, 0], [1, 0], [2, 0], [3, 0],
+                       [0, 1], [1, 1], [2, 1], [3, 1],
+                       [0, 2], [1, 2], [2, 2], [3, 2],
+                       [0, 3], [1, 3], [2, 3], [3, 3] ];
+
+      assert.deepEqual(freeSpaces, expected);
+
+    });
+
+  });
 });
