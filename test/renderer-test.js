3db47624922c4173a3b233815e0c1a48b611d356
mrjaimisra <misrajai01@gmail.com>
2015-10-20 20:06:14 -0600

    Add rowContainer and append 4 rowContainers into gridContainer

--- test/renderer-test.js
+++ test/renderer-test.js
@@ -52,6 +52,14 @@ describe('Renderer', function () {
       assert.equal(renderedBoard.find('.tile-container').length, 1)
     });
 
+    it('should render with four div elements with the class of row-container', function () {
+      let board = new Board();
+      let renderer = new Renderer(board);
+      let renderedBoard = renderer.renderBoard();
+
+      assert.equal(renderedBoard.find('.row-container').length, 4)
+    });
+
   });
 
   describe('renderBoardAndAppendTo', function () {
