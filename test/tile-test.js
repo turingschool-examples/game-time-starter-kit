0b4e488cbe5a783990750388f91ed187b809cb94
mrjaimisra <misrajai01@gmail.com>
2015-10-21 13:48:52 -0600

    Add board and position as properties of tile object

--- test/tile-test.js
+++ test/tile-test.js
@@ -1,10 +1,25 @@
 const assert = require('assert');
+const Tile = require('../lib/tile');
 const Board = require('../lib/board');
 
 describe('the tile', function () {
-  it('should instantiate with a value of 2 or 4', function () {
+  it('should instantiate with a value of 2', function () {
     let tile = new Tile();
 
-    assert.equal(tile.value, (2 || 4));
+    assert.equal(tile.value, 2);
   });
+
+  it('should accept position as an argument', function () {
+    let tile = new Tile([1,3]);
+
+    assert.deepEqual(tile.position, [1,3]);
+  });
+
+  it('should accept a board as an argument', function () {
+    let board = new Board();
+    let tile = new Tile('position', board);
+
+    assert.equal(tile.board, board);
+  });
+
 });
