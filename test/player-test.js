e229f5c17424443f068534bff5bde200a3293d17
Tyler Graham <tylergraham134@gmail.com>
2015-12-09 14:33:55 -0700

    adds testing and implementation for size and position on player, bullet, and meteor

--- test/player-test.js
+++ test/player-test.js
@@ -19,19 +19,20 @@ describe('Player', function() {
     assert.equal(player.board, this.board);
   });
 
-  it('should have an x coordinate passed as the second argument', function() {
-    let player = new Player(this.board, 10);
-    assert.equal(player.x, 10);
-  });
-
-  it('should have a default x coordinate of half the board width', function() {
+  it('should have a default center x coordinate of half the board width', function() {
     let player = new Player(this.board);
-    assert.equal(player.x, this.board.width / 2);
+    assert.equal(player.center.x, this.board.width / 2);
   });
 
-  it('should have a constant y coordinate ten less than board height', function() {
+  it('should have a constant center y coordinate ten less than board height', function() {
     let player = new Player(this.board, 0);
-    assert.equal(player.y, this.board.height-10);
+    assert.equal(player.center.y, this.board.height-10);
+  });
+
+  it('should have a default size of 16 by 16 pixels', function() {
+    let player = new Player(this.board);
+    assert.equal(player.size.x, 16);
+    assert.equal(player.size.y, 16);
   });
 
   it('should be included in the board\'s array of players', function() {
