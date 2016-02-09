ece555ba8e656340a119b270536211dfed595cd9
Emily Dowdle <emily@editingemily.com>
2016-02-09 14:03:20 -0700

    Add sad path tests

--- test/ball-test.js
+++ test/ball-test.js
@@ -28,14 +28,118 @@ describe('Ball', function() {
     });
   });
 
-  describe('move right with defaults', function() {
+  describe('with other values', function() {
+
+    var ball = new Ball({ r: 15,
+                          x: 20,
+                          y: 30,
+                          sAngle: 20,
+                          eAngle: 40
+                        });
+
+    it('should assign an x coordinate', function() {
+      assert.equal(ball.x, 20);
+    });
+
+    it('should assign a radius', function() {
+      assert.equal(ball.r, 15);
+    });
+
+    it('should assign a y coordinate', function() {
+      assert.equal(ball.y, 30);
+    });
+
+    it('should assign a sAngle', function(){
+      assert.equal(ball.sAngle, 0);
+    });
+
+    it('should assign a eAngle', function(){
+      assert.equal(ball.eAngle, 2 * Math.PI);
+    });
+  });
+
+  describe('with zero radius', function() {
+
+    var ball = new Ball({ r: 0 });
+
+    it('should assign an x coordinate', function() {
+      assert.equal(ball.x, 8);
+    });
+
+    it('should assign a radius', function() {
+      assert.equal(ball.r, 8);
+    });
+
+    it('should assign a y coordinate', function() {
+      assert.equal(ball.y, 8);
+    });
+
+    it('should assign a sAngle', function(){
+      assert.equal(ball.sAngle, 0);
+    });
+
+    it('should assign a eAngle', function(){
+      assert.equal(ball.eAngle, 2 * Math.PI);
+    });
+  });
+
+  describe('with negative radius', function() {
+
+    var ball = new Ball({ r: -10 });
+
+    it('should assign an x coordinate', function() {
+      assert.equal(ball.x, 8);
+    });
+
+    it('should assign a radius', function() {
+      assert.equal(ball.r, 8);
+    });
+
+    it('should assign a y coordinate', function() {
+      assert.equal(ball.y, 8);
+    });
+
+    it('should assign a sAngle', function(){
+      assert.equal(ball.sAngle, 0);
+    });
+
+    it('should assign a eAngle', function(){
+      assert.equal(ball.eAngle, 2 * Math.PI);
+    });
+  });
+
+  describe('with negative radius', function() {
+
+    var ball = new Ball({ r: -10 });
+
+    it('should assign an x coordinate', function() {
+      assert.equal(ball.x, 8);
+    });
+
+    it('should assign a radius', function() {
+      assert.equal(ball.r, 8);
+    });
+
+    it('should assign a y coordinate', function() {
+      assert.equal(ball.y, 8);
+    });
+
+    it('should assign a sAngle', function(){
+      assert.equal(ball.sAngle, 0);
+    });
+
+    it('should assign a eAngle', function(){
+      assert.equal(ball.eAngle, 2 * Math.PI);
+    });
+  });
+  
+  describe('draw', function() {
 
     var ball = new Ball({});
 
-    it('x should move one pixel to the right', function() {
+    it('should be drawn', function() {
+      ball.draw
       assert.equal(ball.x, 8);
-      dingus.moveRight()
-      assert.equal(ball.x, 9);
     });
 
   });
--- test/index.js
+++ test/index.js
@@ -1,1 +1,6 @@
 require('./ball-test.js');
+
+const $ = require('jquery');
+
+let canvas = document.getElementById("game");
+let ctx = canvas.getContext('2d');
