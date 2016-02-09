eb03f4d566282c0825fcc3728437fc585c5c373f
Matt Stjernholm <thew88@gmail.com>
2016-02-09 14:23:40 -0700

    tests complete for gate with given values

--- test/gate-test.js
+++ test/gate-test.js
@@ -5,10 +5,11 @@ describe('Gate', function() {
 
   context('with default attributes', function() {
 
+    var canvas = document.getElementById("game");
     var gate = new Gate({});
 
-    it('should have a y coordinate', function() {
-      assert.equal(gate.y, 595);
+    it('should have a y coordinate that is 5 less than the canvas height', function() {
+      assert.equal(gate.y, (canvas.height - 5));
     });
 
     it('should have an x coordinate for where the hole starts', function() {
@@ -17,6 +18,28 @@ describe('Gate', function() {
 
     it('should have an x coordinate for where the hole ends', function() {
       assert.equal(gate.gateEnd, 10);
+    });
+
+  });
+
+  context('with given attributes', function() {
+
+    var canvas = document.getElementById("game");
+    var gate = new Gate({ y: 100,
+                          gateStart: 50,
+                          gateEnd: 100,
+                        });
+    debugger;
+    it('can accept a y coordinate for y position on canvas', function() {
+      assert.equal(gate.y, 100);
+    });
+
+    it('can accept an x coordinate for where the hole starts', function() {
+      assert.equal(gate.gateStart, 50);
+    });
+
+    it('can accept an x coordinate for where the hole ends', function() {
+      assert.equal(gate.gateEnd, 100);
     });
 
   });
