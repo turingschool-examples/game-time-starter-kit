3cb919783b169d502380b89a60616efc3c5126d0
Michael Wong <michael.brian.wong@gmail.com>
2016-02-11 12:07:39 -0700

    Refactoring importing files

--- test/bullet.js
+++ test/bullet.js
@@ -3,21 +3,20 @@ const Bullet = require('../lib/bullet.js');
 const chai = require('chai');
 const assert = chai.assert;
 
-
 describe('Bullet', function() {
    it('has a default x position', function () {
        const bullet = new Bullet({x: 1, y:0}, 1);
        assert.equal(bullet.position.x, 1);
-   })
+   });
 
     it('has a default y position', function () {
         const bullet = new Bullet({x: 1, y:0}, 1);
         assert.equal(bullet.position.y, 0);
-    })
+    });
 
     it('should have a velocity', function () {
         const bullet = new Bullet({x: 1, y: 0}, 1);
         assert.equal(bullet.velocity, 1);
-    })
+    });
 
 });
