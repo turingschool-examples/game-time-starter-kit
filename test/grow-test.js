6a9725a3188c7c65bc706775ca4aed46e2529fc9
Jill Donohue <jillmd501@gmail.com>
2016-02-17 14:52:41 -0700

    0 exceptions

--- test/grow-test.js
+++ test/grow-test.js
@@ -1,19 +1,17 @@
 const assert = require('chai').assert;
 const Snake = require('../lib/snake');
-const Food = require('../lib/food');
-const Game = require('../lib/game');
 
 describe('Snake#grow', function() {
   context('successfully adds', function(){
     beforeEach(function() {
-      this.snake = new Snake({x: 0, y: 0, width: 10, height: 10})
+      this.snake = new Snake({x: 0, y: 0, width: 10, height: 10});
     });
 
     it('block to end', function(){
-      assert.equal(this.snake.y, 0)
+      assert.equal(this.snake.y, 0);
       this.snake.eatFace();
       this.snake.grow();
-      assert.equal(this.snake.direction, 'up')
+      assert.equal(this.snake.direction, 'up');
     });
   });
 });
