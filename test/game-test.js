8f1d7b8d5d9296cf82d5a3ab51897bb846f136d7
Hector Huertas <hectorhuertas@gmail.com>
2016-04-01 22:31:06 -0600

    Test game orienting and moving players

--- test/game-test.js
+++ test/game-test.js
@@ -11,6 +11,7 @@ describe("Game", function(){
     it("has an empty occupiedPositions", function(){
       assert.equal(game.occupiedPositions.length, 0);
     });
+
     it("creates player 1", function(){
       var playerOneX = Math.floor(game.width / 8);
       var playerOneY = Math.floor(game.height / 2);
@@ -18,6 +19,8 @@ describe("Game", function(){
       assert.equal(game.players[0].controls, 'wasd');
       assert.equal(game.players[0].x, playerOneX);
       assert.equal(game.players[0].y, playerOneY);
+      assert.equal(game.players[0].speedX, game.players[0].speed);
+      assert.equal(game.players[0].speedY, 0);
     });
 
     it("creates player 2", function(){
@@ -27,25 +30,46 @@ describe("Game", function(){
       assert.equal(game.players[1].controls, 'arrows');
       assert.equal(game.players[1].x, playerTwoX);
       assert.equal(game.players[1].y, playerTwoY);
+      assert.equal(game.players[1].speedX, -game.players[0].speed);
+      assert.equal(game.players[1].speedY, 0);
     });
   });
 
   context('when running', function(){
+    it("orients players", function(){
+      var game = new Game(100,100);
+      var playerOneSpeedX = game.players[0].speedX;
+      var playerOneSpeedY = game.players[0].speedY;
+      var playerTwoSpeedX = game.players[1].speedX;
+      var playerTwoSpeedY = game.players[1].speedY;
+
+      game.KeyPressed.up = true;
+      game.KeyPressed.w = true;
+      game.update();
+      game.KeyPressed.up = false;
+      game.KeyPressed.w = false;
+
+      assert.notEqual(game.players[0].speedX, playerOneSpeedX);
+      assert.notEqual(game.players[0].speedY, playerOneSpeedY);
+      assert.notEqual(game.players[1].speedX, playerTwoSpeedX);
+      assert.notEqual(game.players[1].speedY, playerTwoSpeedY);
+    });
+
+    it("moves players", function(){
+      var game = new Game(100,100);
+      var playerOnePosition = game.players[0].position();
+      var playerTwoPosition = game.players[1].position();
+
+      game.update();
+
+      assert.notEqual(game.players[0].position(), playerOnePosition);
+      assert.notEqual(game.players[1].position(), playerTwoPosition);
+    });
+
     it("logs positions", function(){
       var game = new Game();
       game.logPosition([12,7]);
       assert(game.occupiedPositions[12][7]);
     });
-
-    it("movePlayers", function(){
-      var game = new Game(100,100);
-      var playerOneX = game.players[0].x;
-
-      game.update();
-
-      assert.notEqual(game.players[0].x, playerOneX);
-    });
-
-    xit("updates the game", function(){});
   });
 });
