7eb2b553e5da4d1015fbada43974c242a2d3b31c
Hector Huertas <hectorhuertas@gmail.com>
2016-04-01 20:20:52 -0600

    Refactor player - Player size depends on speed

--- test/player-test.js
+++ test/player-test.js
@@ -7,26 +7,22 @@ describe("Player", function(){
     it("has default attributes", function(){
       var player = new Player('game', {});
 
+      assert.equal(player.game, 'game');
       assert.equal(player.x, 50);
       assert.equal(player.y, 50);
-      assert.equal(player.height, 10);
-      assert.equal(player.width, 10);
-      assert.equal(player.speed, 3);
-      assert.equal(player.speedX, 3);
+      assert.equal(player.speed, 10);
+      assert.equal(player.size, 10);
+      assert.equal(player.speedX, 10);
       assert.equal(player.speedY, 0);
     });
 
-    it('takes attributes from options', function() {
-      var attributes = {x: 5, y: 44, height: 8, width: 91, speed: 12};
+    it('takes position and speed from options', function() {
+      var attributes = {x: 5, y: 44, speed: 8};
       var player = new Player('game', attributes);
 
       assert.equal(player.x, attributes.x);
       assert.equal(player.y, attributes.y);
-      assert.equal(player.height, attributes.height);
-      assert.equal(player.width, attributes.width);
       assert.equal(player.speed, attributes.speed);
-      assert.equal(player.speedX, attributes.speed);
-      assert.equal(player.speedY, 0);
     });
 
     it("is not dead", function(){
@@ -107,19 +103,19 @@ describe("Player", function(){
 
     it('dies when hitting a wall', function(){
       var player;
-      var game = new Game(20, 20);
+      var game = new Game(200, 200);
 
       player = new Player(game, {x: -1});
-      assert(player.died());
+      assert(player.died(), 'West wall');
 
       player = new Player(game, {y: -1});
-      assert(player.died());
+      assert(player.died(), 'North wall');
 
       player = new Player(game, {x: game.width});
-      assert(player.died());
+      assert(player.died(), 'East wall');
 
       player = new Player(game, {y: game.height});
-      assert(player.died());
+      assert(player.died(), 'South wall');
     });
   });
 });
