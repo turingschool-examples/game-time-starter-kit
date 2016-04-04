998d0203dcdfca5961503a1cebcf518591d89373
Hector Huertas <hectorhuertas@gmail.com>
2016-04-03 20:09:08 -0600

    Adapt fruit to grid
    - Fruit grid depends on grid size
    - Fruit position determined by grid

--- test/fruit-test.js
+++ test/fruit-test.js
@@ -1,24 +1,29 @@
 const assert = require("chai").assert;
 const Fruit = require("../lib/fruit");
+const Game = require("../lib/game");
 
 describe("Fruit", function(){
   context('when created', function(){
-    var fruit = new Fruit(null, 10);
+    var game = new Game(200,200);
+    var fruit = new Fruit(game);
 
     it("has a size", function(){
-      assert.equal(fruit.size, 10);
+      assert(fruit.size);
     });
 
     it("is positioned", function(){
       assert(fruit.x);
       assert(fruit.y);
     });
-
   });
 
   context('when repositioned', function(){
-    xit("its positioned according to the grid", function(){
-      // assert.equal(fruit.occupiedPositions.length, 0);
+    it("its positioned according to the grid", function(){
+      var game = new Game(200,200);
+      var fruit = new Fruit(game);
+
+      assert.equal(fruit.x % game.grid.size, 0);
+      assert.equal(fruit.y % game.grid.size, 0);
     });
 
     xit("is positioned in a free square", function(){
