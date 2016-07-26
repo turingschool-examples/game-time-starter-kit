7a0378ada855510acde627fd93e8488da7a3c0d5
kylem038 <misenckj@gmail.com>
2016-07-26 15:34:56 -0600

    Adding in snake files

---
+++ test/snake-test.js
@@ -0,0 +1,25 @@
+const assert = require('chai').assert;
+
+const Snake = require('../lib/snake');
+
+describe('Snake', function() {
+  context('with default attributes', function() {
+    var snake = new Snake({});
+
+    it('should assign an x coordinate', function() {
+      assert.equal(snake.x, 5);
+    });
+
+    it('should assign a y coordinate', function() {
+      assert.equal(snake.y, 0);
+    });
+
+    it('should assign a height', function(){
+      assert.equal(snake.height, 10);
+    });
+
+    it('should assign a width', function(){
+      assert.equal(snake.width, 10);
+    });
+  });
+});
