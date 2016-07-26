5c16661972f330077d0ba3862d004bf6cd7dbf9a
Andy Young <nxusmg@gmail.com>
2016-07-26 16:01:13 -0600

    add basic files

---
+++ test/grid-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+const Grid = require('../lib/grid');
+
+describe('Grid', function() {
+  it('should be a function', function() {
+    assert.isFunction(Grid);
+  });
+
+
+});
