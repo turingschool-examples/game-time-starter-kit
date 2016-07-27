f2755dd5ff0e91813e98d53c82b4a1a7f7835212
kylem038 <misenckj@gmail.com>
2016-07-27 16:29:49 -0600

    Working on animation

--- test/world-test.js
+++ test/world-test.js
@@ -1,17 +1,36 @@
 const assert = require('chai').assert;
-const world = require('../lib/world');
+const World = require('../lib/world');
 
 describe('world', function () {
 
-  it('should default a default width', function() {
+  it('should start with a default width', function() {
+    var world = new World();
     assert.equal(world.width, 500);
   });
 
-  it('should have a default height', function() {
+  it('should start with a default height', function() {
+    var world = new World();
     assert.equal(world.height, 500);
   });
 
+  it('should be a function', function () {
+    assert.isFunction(World);
+  });
+
+  it('should instantiate an object', function () {
+    var world = new World();
+    assert.isObject(world);
+  });
+
+  // it('should have a "snake" property which starts off as an object', function() {
+  //   var world = new World();
+  //   var snake = new Snake();
+  //   assert.isObject(world,snake);
+  // });
+
 });
+
+
 //WHY?
 //To test edge cases
 //We want to make sure the app works as intended
