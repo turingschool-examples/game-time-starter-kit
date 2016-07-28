10af743527239b647f648d56fdd6c9ac1a124366
kylem038 <misenckj@gmail.com>
2016-07-28 17:08:24 -0600

    working on moving snake on keypress

--- test/snake-in-world-test.js
+++ test/snake-in-world-test.js
@@ -4,7 +4,6 @@ const World = require('../lib/world');
 
 
 describe('Snake in relation to world', function() {
-
   it('should know about the world if it is passed in', function () {
     var world = new World();
     // var snake = new Snake(250,250,10,10,world);
@@ -13,13 +12,17 @@ describe('Snake in relation to world', function() {
   });
 });
 
+// describe('Snake colliding with an object', function() {
+//   it('should have a method called isSnakeColliding()', function () {
+//     var world = new World();
+//     assert.isFunction(world.isSnakeColliding);
+//     });
+// });
+//
+// describe('Snake starting the game', function () {
+//   it('should begin moving by pressing an arrow key', function () {
+//     var snake = world.snake;
+//     assert.isFunction(world.snakeIsMoving);
+//   });
 
-
-
-describe('Snake colliding with an object', function() {
-
-it('should have a method called isSnakeColliding()', function () {
-  var world = new World();
-  assert.isFunction(world.isSnakeColliding);
-  });
-});
+// });
