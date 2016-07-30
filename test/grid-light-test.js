fa5882a4d4c54a663379b6b8675e1bbe979a6366
Andy Young <nxusmg@gmail.com>
2016-07-30 16:49:50 -0600

    Make grid know about the state of light-objects

--- test/grid-light-test.js
+++ test/grid-light-test.js
@@ -3,47 +3,25 @@ const Grid = require('../lib/grid');
 const Light = require('../lib/light');
 
 
+describe('Grid in relation to the Light', function() {
 
+  it('should have a method called addLight()', function() {
+    var grid = new Grid();
+    assert.isFunction(grid.addLight);
+  })
 
+  it('should know about the states of the lights added to the grid', function () {
+    var grid = new Grid();
+    var onLight = new Light(true);
+    var offLight = new Light(false);
 
+    assert.isObject(grid.addLight(true));
+    assert.equal(grid.addLight(true).state, true);
+    assert.equal(grid.addLight(true).state, onLight.state);
+    assert.equal(grid.addLight(false).state, false);
+    assert.equal(grid.addLight(false).state, offLight.state);
+  });
 
+  it('should have a function that creates a new game')
 
-
-
-
-
-
-
-
-
-// describe('Light in relation to the Grid', function() {
-//
-//   it('should know about a grid if it is passed in', function () {
-//     var grid = new Grid(500, 500);
-//     var light = new Light(1, 1, 100, 100, true || false, grid);
-//     assert.equal(light.grid, grid);
-//   });
-//
-//   it('should know about a grid if it is created using grid.addLight()', function() {
-//     var grid = new Grid(500, 500);
-//     var light = new Light(1, 1, 100, 100, true || false);
-//     grid.addLight(light);
-//     assert.equal(light.grid, grid);
-//   });
-//
-// });
-//
-// describe('Grid in relation to the Light', function() {
-//
-//   it('should have a method called addLight()', function() {
-//     var grid = new Grid(500, 500);
-//     assert.isFunction(grid.addLight);
-//   })
-//
-//   it('should know about the lights added to the grid', function (){
-//     var grid = new Grid(500, 500);
-//     var light = new Light(1, 1, 100, 100, true || false);
-//
-//     grid.addLight(light);
-//     assert.include(grid.lights, light);
-//   })
+});
