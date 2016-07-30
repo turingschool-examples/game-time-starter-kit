a174dc61ad10580e18a79c36fed4ce4ea3d1c6e8
Jeff Duke <jeff.duke@gmail.com>
2016-07-30 09:47:14 -0600

    refactored code and tests.  pseudocode next steps

--- test/test-world-paddle.js
+++ test/test-world-paddle.js
@@ -1,11 +1,9 @@
 const chai = require('chai');
 const assert = chai.assert;
 const Paddle = require('../lib/paddle');
-const World = require('../lib/world');
 
-describe('paddle to world', function() {
+describe('Paddle to world', function() {
   it('should stop moving right when it reaches the width of the world', function() {
-    var world = new World(500, 500);
     var paddle = new Paddle({ x: 400 });
     if ((paddle.x + paddle.width) < this.width) {
       paddle.moveRight();
@@ -13,7 +11,6 @@ describe('paddle to world', function() {
     assert.equal(paddle.x, 400);
   });
   it ('should stop moving left when it reaches the left wall of the world', function() {
-    var world = new World(800, 500);
     var paddle = new Paddle({ x: 30});
     if (paddle.x > 0) {
       paddle.moveLeft();
