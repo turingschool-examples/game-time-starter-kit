6c3311b3afaeea55c3b0b55b8fa65c28e0b6a11e
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-11-01 14:40:41 -0600

    updated index files and created test and code files

--- test/index.js
+++ test/index.js
@@ -1,16 +1,1 @@
-var assert = require('chai').assert;
-var Frogger = require('../lib/index');
-
-describe('Frogger', function() {
-
-  it('should have a frog', function() {
-    assert.isFunction(Frogger);
-  });
-
-  it('should instantiate the first round', function(){
-    var frog = new Frogger();
-    assert.isObject(frog);
-  });
-
-  
-})
+require('./frogger-test.js');
