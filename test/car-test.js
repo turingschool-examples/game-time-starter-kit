7749f989513e3bde7804112899e6109de3070c5c
pescel <28l.pesce@gmail.com>
2016-11-07 15:33:10 -0700

    Add car tests

--- test/car-test.js
+++ test/car-test.js
@@ -7,24 +7,39 @@ describe('Cars', function() {
     assert.isFunction(Car);
   });
 
-  it('"Cars" should take a variable for x and y', function() {
-    var car = new Car();
+  it('"Cars" should take a variable for x', function() {
+    var car = new Car(150, 200);
     assert.equal(car.x, 150);
+  });
+
+  it('"Cars" should take a variable for y', function() {
+    var car = new Car(150, 200);
     assert.equal(car.y, 200);
   });
 
-  it('should have a method called "carDraw" that puts it on the canvas', function() {
-    var car = new Car();
-    assert.isFunction(car.carDraw);
+  it('should have a method called "carDraw1" that puts car1 on the canvas', function() {
+    var car = new Car(150, 200);
+    assert.isFunction(car.carDraw1);
+  });
+
+  it('should have a method called "carDraw2" that puts car2 on the canvas', function() {
+    var car = new Car(150, 200);
+    assert.isFunction(car.carDraw2);
+  });
+
+  it('should have a method called "carDraw3" that puts car3 on the canvas', function() {
+    var car = new Car(150, 200);
+    assert.isFunction(car.carDraw3);
+  });
+
+  it('should have a method called "carDraw4" that puts car4 on the canvas', function() {
+    var car = new Car(150, 200);
+    assert.isFunction(car.carDraw4);
   });
 
   it('should have a method called "carMove"', function() {
-    var car = new Car
+    var car = new Car(150, 200);
     assert.isFunction(car.carMove);
   });
 
-  // it('"Cars", should have a width, height, and color', function(){
-  //   var car = new Car();
-  //   assert.equal(car.width, width);
-  // });
-})
+});
